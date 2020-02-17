4dece094eabce880b569114b7338cb3e2b2f801c
Vlad Chernov <vladchernov12@yandex.ru>
2020-02-17 23:11:13 +0400

    in self-task\work_12.02.2020 add projects\chernov_vector

--- self_task/work_12.02.2020/projects/chernov_vector/chernov_vector/Program.cs
+++ self_task/work_12.02.2020/projects/chernov_vector/chernov_vector/Program.cs
@@ -6,7 +6,22 @@ namespace chernov_vector
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Vector vector1 = new Vector(2,3,5);
+            Vector vector2 = new Vector(3,6,7);
+
+            Console.WriteLine("vec1" + vector1.ToString());
+            Console.WriteLine("vec2" + vector2.ToString());
+
+            vector1.Length();
+            Console.WriteLine($"Результат длины вектора: {vector1.Length()}");
+
+            vector1.ScalarProduct(vector2);
+            Console.WriteLine($"Результат скалярного произведения векторов: {vector1.ScalarProduct(vector2)}");
+
+            vector1.VectorArtWork(vector2);
+            Console.WriteLine($"Результат векторного произведения векторов: {vector1.VectorArtWork(vector2)}");
+
+            Console.ReadKey();
         }
     }
 }
---
+++ self_task/work_12.02.2020/projects/chernov_vector/chernov_vector/Vector.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace chernov_vector
+{
+    class Vector
+    {
+        private double X { get; set; }
+
+        private double Y { get; set; }
+
+        private double Z { get; set; }
+
+        public Vector(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public double Length()
+        {
+            double L = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2) + Math.Pow(Z, 2));
+            return L;
+        }
+
+        public double ScalarProduct(Vector vector2)
+        {
+            double S = X * vector2.X + Y * vector2.Y + Z * vector2.Z;
+            return S;
+        }
+
+        public Vector VectorArtWork(Vector vector2)
+        {
+            double x = (Y * vector2.Z * vector2.Y);
+            double y = (Z * vector2.X * vector2.Z);
+            double z = (X * vector2.Y * vector2.X);
+
+            Vector vector3 = new Vector(x, y, z);
+            return vector3;
+        }
+
+        public override string ToString()
+        {
+            return ($"(x = {X}, y = {Y}, z = {Z})");
+        }
+    }
+}
